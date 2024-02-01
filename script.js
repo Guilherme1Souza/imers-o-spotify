32e3ee63679ae3c21f07b7d5994fbbe4ce44bd37
Guilherme Eduardo <guilhermeeduardo2016@outlook.com>
2024-02-01 00:58:57 -0300

    feat - fechamento do javascript e API

--- script.js
+++ script.js
@@ -1,26 +1,37 @@
-const searchInput = document.getElementById('search-input'); 
-const ResultArtists = document.getElementById('result-artist');
+const searchInput = document.getElementById('search-input');
+const resultArtists = document.getElementById('result-artist');
 const resultPlaylist = document.getElementById('result-playlists');
-
 
 function requestApi(searchTerm) {
     const url = `http://localhost:3000/artists?name_like=${searchTerm}`
+
     fetch(url)
-    .then((response)=> response.json())
-  //  .then((result) => displayResults(result))
+        .then((response) => response.json())
+        .then((result) => displayResults(result))
 }
 
+function displayResults(result) {
+    resultPlaylist.classList.add("hidden")
 
+    const artistName = document.getElementById('artist-name');
+    const artistImage = document.getElementById('artist-img');
 
-document.addEventListener('input', function() {
+    result.forEach(element => {
+        artistName.innerText = element.name;
+        artistImage.src = element.urlImg;
+    });
+
+    resultArtists.classList.remove('hidden'); 
+}
+
+document.addEventListener('input', function () {
     const searchTerm = searchInput.value.toLowerCase();
     if (searchTerm === '') {
         resultPlaylist.classList.add('hidden');
-        ResultArtists.classList.remove('result-artist');
-        return;
+        resultArtists.classList.remove('hidden');
+        return
     }
 
     requestApi(searchTerm);
-
-
 });
+
